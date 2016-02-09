937f09cc6a5c1d3069eae5acc038be349640e23e
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-02-09 22:50:58 +0000

    user restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -15,12 +16,11 @@ class UsersController < ApplicationController
       flash[:success] = "Welcome to the alpha blog #{@user.username}"
       redirect_to articles_path
     else
-      render 'new'
+    render 'new'
     end
   end
   
   def edit
-  
   end
   
   def update
@@ -44,5 +44,12 @@ class UsersController < ApplicationController
   
   def set_user
     @user = User.find(params[:id])
+  end
+  
+  def require_same_user
+    if current_user != @user
+      flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
   end
 end
